5462375c4476d9eeed12be0ae83c11ea69ad2f6e
wtakaman <takaman@gmail.com>
2022-10-23 21:40:49 +0300

    first  commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x68fd72C8Bdbb3C6cE9F11D84f416916F68eac621";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://takaman-nft-mint-dapp.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require('@nomiclabs/hardhat-waffle');
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: 'INFURA_URL',
+      accounts: ['PRIVATE_KEY'],
+    },
+  },
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  mocha: {
+    timeout: 40000,
+  },
+};
